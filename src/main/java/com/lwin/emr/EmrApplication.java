b13f2d0f2345c5c3830b6aef7d0cc3eb9281c692
Lwin <lwinmoethu25@gmail.com>
2022-10-21 21:42:57 +0800

    Add Dependencies

--- src/main/java/com/lwin/emr/EmrApplication.java
+++ src/main/java/com/lwin/emr/EmrApplication.java
@@ -2,12 +2,26 @@ package com.lwin.emr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @SpringBootApplication
 public class EmrApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmrApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI generateOpenAPI() {
+		return new OpenAPI()
+				.info(new Info().title("LWIN EMR")
+						.description("Assignment")
+						.version("v1.0.0")
+						.license(new License().name("Apache 2.0").url("http://springdoc.org")));
 	}
 
 }
